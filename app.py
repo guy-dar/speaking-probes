d819d673127fa561ee7933a3f9452239f2e6548e
guyd1995 <57269082+guyd1995@users.noreply.github.com>
2023-01-19 00:45:18 +0200

    add sidebar to app

--- app.py
+++ app.py
@@ -21,6 +21,13 @@ model, model_params, tokenizer = load_model(model_name)
 # neuron_dim = col3.text_input("Dim: ", value='0')
 # neurons = model_params.K_heads[int(neuron_layer), int(neuron_dim)]
 
+with st.sidebar:
+    temperature = st.slider("Temperature", min_value=0., max_value=2., value=0.5, step=0.05)
+    repetition_penalty = st.slider("Repetition Penalty", min_value=0., max_value=4., value=2., step=0.1)
+    sidebar_cols = st.columns(2)
+    num_generations = sidebar_cols[0].number_input("Number of Answers", min_value=1, value=3, format='%d')
+    max_new_tokens = sidebar_cols[1].number_input("Max Answer Length", min_value=1, value=50, format='%d')
+
 prompt = st.text_area("Prompt: ")
 submitted = st.button("Send!")
 
@@ -28,9 +35,9 @@ if submitted:
     with st.spinner('Wait for it..'):
         model, model_params, tokenizer = map(deepcopy, (model, model_params, tokenizer))
         decoded = speaking_probe(model, model_params, tokenizer, prompt, 
-                                 repetition_penalty=2., num_generations=3,
+                                 repetition_penalty=repetition_penalty, num_generations=num_generations,
                                  min_length=1, do_sample=True, 
-                                 max_new_tokens=100)
+                                 max_new_tokens=max_new_tokens, temperature=temperature)
 
     for text in decoded:
         st.code('\n'.join(textwrap.wrap(text, width=70)), language=None)
