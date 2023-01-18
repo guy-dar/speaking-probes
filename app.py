4c0add76d63b84d876219effe9edcc23b8168ced
guyd1995 <57269082+guyd1995@users.noreply.github.com>
2023-01-19 00:12:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,23 +14,23 @@ def load_model(model_name):
     return model, model_params, tokenizer
     
 
-col1, col2, col3, *_ = st.columns(5)
+col1, col2, col3, *_ = st.columns(4)
 model_name = col1.selectbox("Select a model: ", options=['gpt2', 'gpt2-medium', 'gpt2-large'])
 model, model_params, tokenizer = load_model(model_name)
-neuron_layer = col2.text_input("Layer: ", value='0')
-neuron_dim = col3.text_input("Dim: ", value='0')
+# neuron_layer = col2.text_input("Layer: ", value='0')
+# neuron_dim = col3.text_input("Dim: ", value='0')
+# neurons = model_params.K_heads[int(neuron_layer), int(neuron_dim)]
 
-neurons = model_params.K_heads[int(neuron_layer), int(neuron_dim)]
 prompt = st.text_area("Prompt: ")
 submitted = st.button("Send!")
 
 if submitted:
     with st.spinner('Wait for it..'):
         model, model_params, tokenizer = map(deepcopy, (model, model_params, tokenizer))
-        decoded = speaking_probe(model, model_params, tokenizer, prompt, *neurons, 
+        decoded = speaking_probe(model, model_params, tokenizer, prompt, 
                                  repetition_penalty=2., num_generations=3,
                                  min_length=1, do_sample=True, 
                                  max_new_tokens=100)
 
     for text in decoded:
-        st.text(textwrap.wrap(text, width=70))
+        st.code('\n'.join(textwrap.wrap(text, width=70)), language=None)
